0058effdb291ef46bfc010bc023b351a2fe46068
juliana <juliraymondi@gmail.com>
2025-07-05 13:14:49 +0100

    Init web project

---
+++ ASP.NET_MVC_01_NET8.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_MVC_01_NET8.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
